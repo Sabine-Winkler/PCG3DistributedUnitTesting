97e6010b43053f30cce9316df2ee481e73a5dcca
sabinewinkler <sabine.winkler@students.fh-hagenberg.at>
2015-12-18 18:26:46 +0100

    Adapted GUI. Now, the assembly can be deployed to the servers.

---
+++ DistUnitTesting/PCG3.Client.Logic/ClientLogic.cs
@@ -0,0 +1,35 @@
+﻿using PCG3.Middleware;
+using System;
+using System.IO;
+using XcoAppSpaces.Core;
+
+namespace PCG3.Client.Logic {
+
+  public class ClientLogic {
+
+    private const string APP_SPACE_CONFIG_STRING = "tcp.port=0";
+
+    public XcoAppSpace Space { get; private set; }
+
+    public ClientLogic() {
+      this.Space = new XcoAppSpace(APP_SPACE_CONFIG_STRING);
+    }
+
+
+    public void DeployAssemblyToServer(string assemblyPath, string serverAddress) {
+
+      
+      AssemblyWorker worker = Space.ConnectWorker<AssemblyWorker>(serverAddress);
+      AssemblyRequest request = new AssemblyRequest();
+      request.Bytes = File.ReadAllBytes(assemblyPath);
+      request.ResponsePort = Space.Receive<AssemblyResponse>(resp => {
+        if (!resp.Worked) {
+          // print error message if assembly deployment failed
+          Console.WriteLine(resp.ErrorMsg);
+        }
+      });
+
+      worker.Post(request);
+    }
+  }
+}
