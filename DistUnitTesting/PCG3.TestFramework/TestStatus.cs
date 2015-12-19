71fbe321c7993be44c06bb9cd0e59b20da2ff716
sabinewinkler <sabine.winkler@students.fh-hagenberg.at>
2015-12-19 18:14:30 +0100

    Added comments to the test status.

--- DistUnitTesting/PCG3.TestFramework/TestStatus.cs
+++ DistUnitTesting/PCG3.TestFramework/TestStatus.cs
@@ -2,9 +2,24 @@
 
   public static class TestStatus {
 
+    /// <summary>
+    /// The test is available to be send to a server.
+    /// </summary>
     public const string NONE    = "None";
+
+    /// <summary>
+    /// The test failed.
+    /// </summary>
     public const string FAILED  = "Failed";
+
+    /// <summary>
+    /// The test was successful. It passed.
+    /// </summary>
     public const string PASSED  = "Passed";
+
+    /// <summary>
+    /// The test was sent to a server and it is waiting for the result now.
+    /// </summary>
     public const string WAITING = "Waiting...";
   }
 }
