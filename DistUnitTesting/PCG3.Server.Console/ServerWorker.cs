f972b63b56aa56ecfbcc68dd8ec4f4737abe544d
alasinger <astrid.lasinger@gmail.com>
2015-12-19 15:34:04 +0100

    added TestWorker - Test (won't work)

--- DistUnitTesting/PCG3.Server.Console/ServerWorker.cs
+++ DistUnitTesting/PCG3.Server.Console/ServerWorker.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using XcoAppSpaces.Core;
 using System.Reflection;
+using PCG3.TestFramework;
 
 namespace PCG3.Server {
   class ServerAssemblyWorker : AssemblyWorker {
@@ -18,6 +19,7 @@ namespace PCG3.Server {
       try {
         Assembly.Load(assembly.Bytes);
         response.Worked = true;
+        Console.WriteLine(assembly.Bytes);
       }
       catch (Exception e) {
         response.Worked = false;
@@ -26,6 +28,9 @@ namespace PCG3.Server {
       finally {
         assembly.ResponsePort.Post(response);
       }
+
+
+      
     }
   }
 
@@ -33,22 +38,47 @@ namespace PCG3.Server {
   public class ServerTestWorker : TestWorker {
 
     public int Cores { get; set; }
+    public List<Task> Tasks { get; set; }
 
-    public ServerTestWorker(int Cores) {
-      this.Cores = Cores;
-    }
+    //public ServerTestWorker(int Cores) {
+    //  //this.Cores = Cores;
+    //  //for (int i = 1; i <= Cores; i++) {
+    //  //  Tasks.Add(new Task())
+    //  //}
+    //}
 
     [XcoConcurrent]
     public void Process(TestRequest test) {
       var response = new TestResponse();
 
-      //Test ausführen
+      TestRunner tr = new TestRunner();
+      
+      string assemblyName = @"F:\\MSc\\3_PCG\\UET2\\PCG3.TestUnitTests.dll";
+      Assembly assembly = Assembly.LoadFrom(assemblyName);
+      TestResults results = tr.RunTests(assembly);
+      Console.WriteLine(results);
 
+     
       
 
-      //Tasks (nicht Threads)
-      //waitany
-      
+     // //Test ausführen
+     // for (int i = 1; i<= Cores; i++) {
+     //   Tasks.Add
+     // }
+
+     // //Tasks (nicht Threads)
+     // //waitany
+     //// for 
+     // Task t = Task.Run( () => {
+     //   // Just loop.
+     //   int ctr = 0;
+     //   for (ctr = 0; ctr <= 1000000; ctr++)
+     //                             {}
+     //   Console.WriteLine("Finished {0} loop iterations",
+     //                                               ctr);
+     // } );
+     // t.Wait();
+
 
       //response.result = Testresult;
       test.ResponsePort.Post(response);
