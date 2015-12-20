c82069db4e27be1783e56fb7df3aa5436aa8f0a9
sabinewinkler <sabine.winkler@students.fh-hagenberg.at>
2015-12-20 20:58:40 +0100

    Added a utility class to get the current date time as formatted string.

---
+++ DistUnitTesting/PCG3.Util/DateUtil.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PCG3.Util {
+
+  public static class DateUtil {
+
+    private const string DATE_TIME_TEMPLATE = "[{0}]";
+    private const string DATE_TIME_FORMAT = "yyyyMMdd-HH:mm:ss.fff";
+
+    public static string GetCurrentDateTime() {
+      return string.Format(DATE_TIME_TEMPLATE, DateTime.Now.ToString(DATE_TIME_FORMAT));
+    }
+  }
+}
