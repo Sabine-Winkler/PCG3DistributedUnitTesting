cfd7f0b5d73137cecfdaca36fe424e1d398fd3f7
sabinewinkler <sabine.winkler@students.fh-hagenberg.at>
2015-12-23 14:25:37 +0100

    Replaced the long paths of the console windows by shorter, more meaningful ones.

--- DistUnitTesting/run.bat
+++ DistUnitTesting/run.bat
@@ -17,10 +17,10 @@ set "SERVER2=localhost:%SERVER2_PORT%"
 
 rem === start servers (server1, server2)
 echo "Start server1 ..."
-start %PROGRAM_SERVER% %SERVER1_PARALLEL_DEGREE% %SERVER1_PORT%
+start "Server1 (%SERVER1%, %SERVER1_PARALLEL_DEGREE% cores)" %PROGRAM_SERVER% %SERVER1_PARALLEL_DEGREE% %SERVER1_PORT%
 echo "Start server2 ..."
-start %PROGRAM_SERVER% %SERVER2_PARALLEL_DEGREE% %SERVER2_PORT%
+start "Server2 (%SERVER2%, %SERVER2_PARALLEL_DEGREE% cores)" %PROGRAM_SERVER% %SERVER2_PARALLEL_DEGREE% %SERVER2_PORT%
 
 rem === start client
 echo "Start client ..."
-start %PROGRAM_CLIENT% %ASSEMBLY% %SERVER1% %SERVER2%
+start "Client" %PROGRAM_CLIENT% %ASSEMBLY% %SERVER1% %SERVER2%
